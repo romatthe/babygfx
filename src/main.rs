2bf913476ef3e62a0353ebee2af128172ee93899
Robin Mattheussen <me@romatthe.dev>
2023-08-18 01:59:58 +0200

    Move setup code

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
-use sdl2::{self, video::{self, WindowPos, Window}, event::Event, keyboard::Keycode, render::Canvas, pixels::Color};
+use sdl2::{self, video::{Window}, event::Event, keyboard::Keycode, render::Canvas, pixels::Color, Sdl};
 
 fn main() {
-    // Initialize the SDL context
-    let sdl_context = sdl2::init().unwrap_or_else(|err| panic!("Failed to initialize SDL: {}", err));
-    // Initialize the SDL video subsystem
-    let video_subsystem = sdl_context.video().unwrap_or_else(|err| panic!("SDL video subsystem failed to initialize: {}", err));
-
-    // Create an SDL window
-    let window = video_subsystem.window("BabyGFX", 800, 600)
-        .position_centered()
-        // .borderless()
-        .build()
-        .unwrap_or_else(|err| panic!("Error creating an SDL window: {}", err));
-
-    // Create a canvas with a renderer
-    let mut canvas = window.into_canvas()    
-        .build()
-        .unwrap();
-
-    let mut event_pump = sdl_context.event_pump().unwrap();
-
     // Setup
+    let (context, mut canvas) = setup();
+    let mut event_pump = context.event_pump().unwrap();
 
+    // Main "game" loop
     'running: loop {
         // Process input
         for event in event_pump.poll_iter() {
@@ -41,6 +25,26 @@ fn main() {
         // Render
         render(&mut canvas)
     }
+}
+
+fn setup() -> (Sdl, Canvas<Window>) {
+    // Initialize the SDL context and video subsystem
+    let sdl_context = sdl2::init().unwrap_or_else(|err| panic!("Failed to initialize SDL: {}", err));
+    let video_subsystem = sdl_context.video().unwrap_or_else(|err| panic!("SDL video subsystem failed to initialize: {}", err));
+
+    // Create an SDL window
+    let window = video_subsystem.window("BabyGFX", 800, 600)
+        .position_centered()
+        // .borderless()
+        .build()
+        .unwrap_or_else(|err| panic!("Error creating an SDL window: {}", err));
+
+    // Create a canvas with a renderer
+    let mut canvas = window.into_canvas()    
+        .build()
+        .unwrap();
+
+    (sdl_context, canvas)
 }
 
 fn process_input(event: Event) {
