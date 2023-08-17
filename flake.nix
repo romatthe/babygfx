9dda0f3ac81dd19180e4c32934bab22def1f0051
Robin Mattheussen <me@romatthe.dev>
2023-08-17 23:38:08 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "Baby's first software renderer";
+   
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, flake-compat, nixpkgs, rust-overlay, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        pkg-version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+        rust-version = "1.71.0";
+        rust-dist = pkgs.rust-bin.stable.${rust-version}.default.override {
+          extensions = [ "clippy" "rust-src" "rustfmt" "rust-analyzer" ];
+          targets = [ "x86_64-unknown-linux-gnu" ];
+        };
+      in {
+        defaultPackage = with pkgs; rustPlatform.buildRustPackage {
+          pname = "babygfx";
+          version = pkg-version;
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+	  buildInputs = [
+	    SDL2
+	  ];
+        };
+  
+        defaultApp = utils.lib.mkApp {
+          drv = self.defaultPackage."${system}";
+        };
+
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            # Project tools
+            cargo-msrv
+            nixfmt
+            rust-dist
+
+	    # Deps
+	    SDL2
+          ];
+
+          shellHook = ''
+  
+          '';
+  
+          RUST_BACKTRACE = "1";
+        };
+      }
+    );
+  }
